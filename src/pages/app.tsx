939ac1f043537598d45eb1a5f664d127272db41b
sadness_ojisan <sadness.ojisan@gmail.com>
2021-01-31 19:53:47 +0900

    signup

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -3,6 +3,7 @@ import { Route, Router } from "preact-router";
 
 import { Root } from "./root";
 import { Signin } from "./signin";
+import { Signup } from "./signup";
 import { User } from "./user";
 
 export const App = () => {
@@ -10,6 +11,7 @@ export const App = () => {
     <Router>
       <Route path="/users/:id" component={User} />
       <Route path="/signin" component={Signin} />
+      <Route path="/signup" component={Signup} />
       <Route path="/" component={Root} />
     </Router>
   );
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,5 +1,20 @@
 import { h } from "preact";
 
 export const Signup = () => {
-  return <div>signup</div>;
+  return (
+    <div>
+      <h1>sign up</h1>
+      <button>github signup</button>
+      <form>
+        <div>
+          <label>email</label>
+          <input type="email" />
+        </div>
+        <div>
+          <label>password</label>
+          <input type="password" />
+        </div>
+      </form>
+    </div>
+  );
 };
