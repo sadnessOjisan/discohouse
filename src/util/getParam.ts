7a53a930e4755de669aac0a113dec36d85aa56b8
sadness_ojisan <sadness.ojisan@gmail.com>
2021-02-01 00:05:03 +0900

    fix format

--- src/util/getParam.ts
+++ src/util/getParam.ts
@@ -1,7 +1,7 @@
 export function getParam(name: string, url: string) {
   if (!url) url = window.location.href;
   name = name.replace(/[\[\]]/g, "\\$&");
-  const regex = new RegExp(`[?&]${  name  }(=([^&#]*)|&|#|$)`),
+  const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`),
     results = regex.exec(url);
   if (!results) return null;
   if (!results[2]) return "";
